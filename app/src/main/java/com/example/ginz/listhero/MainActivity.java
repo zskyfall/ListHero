176af42e83d99771a00c4d81396474f2525d4d67
Thangdaik <zefost@gmail.com>
2018-07-13 09:03:37 +0700

    builing recyclerView

--- app/src/main/java/com/example/ginz/listhero/MainActivity.java
+++ app/src/main/java/com/example/ginz/listhero/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.ginz.listhero;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerViewAdapter mRecyclerViewAdapter;
+    private List<String> mDataTitle;
+    private List<Integer> mDataAvatar;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mDataTitle = new ArrayList<>();
+        mDataAvatar = new ArrayList<>();
+        mDataTitle.add("Black Panther");
+        mDataTitle.add("Iron Man");
+        mDataTitle.add("Spider Man");
+        mDataAvatar.add(R.drawable.panther);
+        mDataAvatar.add(R.drawable.ironman);
+        mDataAvatar.add(R.drawable.spiderman);
+
+        mRecyclerViewAdapter = new RecyclerViewAdapter(mDataTitle, mDataAvatar);
+
+        mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
     }
 }
